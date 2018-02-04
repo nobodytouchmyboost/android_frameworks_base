f6d25a3ecc0a40b4bfda302862c99bc088afb19a
maxwen <max.weninger@gmail.com>
2018-02-04 14:00:33 +0100

    base: pass WindowPolicy to DeviceKeyHandler
    so that it can be used from there for e.g. haptic feedback
    
    Change-Id: If010f3e61fbe74bddf87eb96acbf186413e1994c

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -2399,6 +2399,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                 Constructor<?> constructor = klass.getConstructor(Context.class);
                 mDeviceKeyHandler = (DeviceKeyHandler) constructor.newInstance(
                         mContext);
+                mDeviceKeyHandler.setWindowManagerPolicy(this);
                 if(DEBUG) Slog.d(TAG, "Device key handler loaded");
             } catch (Exception e) {
                 Slog.w(TAG, "Could not instantiate device key handler "
